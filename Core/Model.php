181a82db0e84fd311edf2984144efc0d530740fa
harrycampaz <harrycampaz@gmail.com>
2019-02-15 18:33:07 -0500

    Framework base

--- Core/Model.php
+++ Core/Model.php
@@ -3,7 +3,7 @@
 namespace Core;
 
 use PDO;
-
+use App\Config;
 /**
  * Base model
  *
@@ -22,14 +22,10 @@ abstract class Model
         static $db = null;
 
         if ($db === null) {
-            $host = 'localhost';
-            $dbname = 'mvc';
-            $username = 'root';
-            $password = '';
-    
+        
             try {
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", 
-                              $username, $password);
+                $db = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_NAME.";charset=utf8", 
+                Config::DB_USER, Config::DB_PASSWORD);
 
             } catch (PDOException $e) {
                 echo $e->getMessage();
